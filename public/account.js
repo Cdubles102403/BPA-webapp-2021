0add481acd388dd8106fbbc6029f75ff9159d71b
Cody <33189090+cdubles@users.noreply.github.com>
2022-01-15 20:57:10 -0500

    quick fix

--- public/account.js
+++
@@ -1,9 +0,0 @@
-$(function(){
-    //load header
-    $('div#header').load('header.html')
-    let token  = localStorage.getItem('token');
-    console.log(token)
-    if(token == null){
-        window.location.href = "/login.html"
-    }
-})
